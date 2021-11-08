019b09500daca5358b38da65a91c0e69805814d9
dgoceva <dgoceva@abv.bg>
2021-11-08 19:43:28 +0200

    save and read image with python

---
+++ python_DB_Sample/read_image.py
@@ -0,0 +1,32 @@
+import pymysql
+
+
+def write_file(data, filename):
+    # Convert binary data to proper format and write it on Hard Disk
+    with open(filename, 'wb') as file:
+        file.write(data)
+
+
+con = pymysql.connect(host='localhost', user='user7',
+                      password='s$cret', database='testdb')
+
+try:
+    with con.cursor() as cursor:
+        cursor.execute("SELECT * FROM employee WHERE id=%s", 1)
+        record = cursor.fetchall()
+        for row in record:
+            print("Id = ", row[0], )
+            print("Name = ", row[1])
+            image = row[2]
+            file = row[3]
+            print("Storing employee image and bio-data on disk \n")
+            write_file(image, 'Winnie-the-Pooh-out.jpg')
+            write_file(file, 'lorem-ipsum-out.txt')
+
+except con.Error as error:
+    print("Failed to read BLOB data from table {}".format(error))
+
+finally:
+    cursor.close()
+    con.close()
+    print("DB connection is closed")
---
+++ python_DB_Sample/save_image.py
@@ -0,0 +1,29 @@
+import pymysql
+
+
+def convertToBinaryData(filename):
+    # Convert digital data to binary format
+    with open(filename, 'rb') as file:
+        binaryData = file.read()
+    return binaryData
+
+
+con = pymysql.connect(host='localhost', user='user7',
+                      password='s$cret', database='testdb')
+
+try:
+    with con.cursor() as cursor:
+        cursor.execute("""INSERT INTO employee
+            (id, name, photo, biodata) VALUES (%s,%s,%s,%s)""",
+                       (1, 'Winnie the Pooh',
+                        convertToBinaryData('Winnie-the-Pooh.jpg'),
+                        convertToBinaryData('lorem-ipsum.txt')))
+    con.commit()
+    print('Image and file inserted successfully as a BLOB into employee table')
+except con.Error as error:
+    print("Failed inserting BLOB data into table {}".format(error))
+
+finally:
+    cursor.close()
+    con.close()
+    print("DB connection is closed")
