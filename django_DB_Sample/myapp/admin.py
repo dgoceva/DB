12a80fe00c5366004cc9f1f3426d1df3db2826e0
dgoceva <dgoceva@abv.bg>
2021-11-17 16:25:45 +0200

    admin page added

--- django_DB_Sample/myapp/admin.py
+++ django_DB_Sample/myapp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from myapp.models import Name
 
 # Register your models here.
+admin.site.register(Name)
--- django_DB_Sample/myapp/views.py
+++ django_DB_Sample/myapp/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from myapp.models import Name
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'index.html', {})
+    names_from_db = Name.objects.all()
+    context_dict = {'names_from_context': names_from_db}
+    return render(request, 'index.html', context_dict)
